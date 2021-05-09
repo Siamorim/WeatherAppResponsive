1783eefeecbcbee4107bbfc94152dd16b31f2364
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-05-10 00:25:09 +0100

    changes

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -8,10 +8,6 @@ export default function weatherApp() {
     <div className="container-fluid">
       <div className="card">
         <Weather defaultCity="Lisbon" />
-        <div className="title">
-          The
-          <div className="titleSpace">Weather</div>
-        </div>
       </div>
       <footer className="code">
         <span> Open-source code by Sílvia Amorim, on </span>
